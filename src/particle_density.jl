8ee4de0fd13816a8ed63f00d465588663f19e9f5
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-11-12 13:31:47 +0100

    Refactor qp_tensors function for clarity and improve measure_qp error handling

--- src/particle_density.jl
+++ src/particle_density.jl
@@ -1,52 +1,66 @@
-function qp_tensors(sites::ITensors.Indices)	
-	 L = let L = zeros(2, 2, 2, 2)# s1', s2', s1, s2
-		L[2, 1, 2, 1] = 2
-		L[2, 2, 2, 2] = 1
-		L
-	end
+"""
+Construct the tensors needed to measure the quasiparticle density.
+The local number of quasiparticles is measured by using 3-site tensors.
+On the edges, 2-site tensors are used, they are obtained by
+imagining that there are virtual sites outside of the chain in the |0⟩ state.
+"""
 
-	 C = let C = zeros(2, 2, 2, 2, 2, 2)# s1' s2' s3' s1 s2 s3
-		C[1, 2, 1, 1, 2, 1] = 2
-		C[1, 2, 2, 1, 2, 2] = 1
-		C[2, 2, 2, 2, 2, 2] = 1
-		C
-	end
-	
-	 R = let R = zeros(2, 2, 2, 2)# s1' s2' s1 s2
-		R[1, 2, 1, 2] = 2
-		R
-	end
 
-	 N = length(sites)
+"""
+	qp_tensors(sites::ITensors.Indices)::Vector{ITensor}
+Construct the tensors needed to measure the quasiparticle density.
+The convention for the indices order is that output site indices come first.
+"""
+function qp_tensors(sites::ITensors.Indices)
+    leftmost = let L = zeros(2, 2, 2, 2)# s1', s2', s1, s2
+        L[2, 1, 2, 1] = 2
+        L[2, 2, 2, 2] = 1
+        L
+    end
 
-	res = Vector{ITensor}(undef, N)
-	res[1] = itensor(L, sites[1]', sites[2]', dag(sites[1]), dag(sites[2]))
-	for i in 2:N-1
-		res[i] = itensor(C, sites[i-1]', sites[i]', sites[i+1]', dag(sites[i-1]), dag(sites[i]), dag(sites[i+1]))
-	end
-	res[N] = itensor(R, sites[N-1]', sites[N]', dag(sites[N-1]), dag(sites[N]))
-	return res
+    central = let C = zeros(2, 2, 2, 2, 2, 2)# s1' s2' s3' s1 s2 s3
+        C[1, 2, 1, 1, 2, 1] = 2
+        C[1, 2, 2, 1, 2, 2] = 1
+        C[2, 2, 2, 2, 2, 2] = 1
+        C
+    end
+
+    rightmost = let R = zeros(2, 2, 2, 2)# s1' s2' s1 s2
+        R[1, 2, 1, 2] = 2
+        R
+    end
+
+    N = length(sites)
+
+    res = Vector{ITensor}(undef, N)
+    res[1] = itensor(leftmost, sites[1]', sites[2]', dag(sites[1]), dag(sites[2]))
+    for i in 2:N-1
+        res[i] = itensor(central, sites[i-1]', sites[i]', sites[i+1]', dag(sites[i-1]), dag(sites[i]), dag(sites[i+1]))
+    end
+    res[N] = itensor(rightmost, sites[N-1]', sites[N]', dag(sites[N-1]), dag(sites[N]))
+    return res
 end
 
+"""
+	measure_qp(ψ::MPS, tensors::Vector{ITensor})::Float64
+Measure the quasiparticle density of the MPS `ψ` using the provided
+`tensors` obtained from `qp_tensors`.
+"""
 function measure_qp(ψ::MPS, tensors::Vector{ITensor})
-	s = 0
-	for (i, tensor) in enumerate(tensors)
-		ψ = orthogonalize(ψ, i)
-		T = let
-			if i == 1
-				ψ[i] * ψ[i+1]
-			elseif i == length(ψ)
-				ψ[i-1] * ψ[i]
-			else
-				ψ[i-1] * ψ[i] * ψ[i+1]
-			end
-		end
-		try
-			s += scalar(prime(T, tags="Site") * (tensor * T))
-		catch e
-			@error "Error in measure_qp: " i T tensor
-			rethrow(e)
-		end
-	end
-	return real(s)
+    s = 0
+    for (i, tensor) in enumerate(tensors)
+        ψ = orthogonalize(ψ, i)
+        T = let
+            if i == 1
+                ψ[i] * ψ[i+1]
+            elseif i == length(ψ)
+                ψ[i-1] * ψ[i]
+            else
+                ψ[i-1] * ψ[i] * ψ[i+1]
+            end
+        end
+        s += scalar(T * apply(tensor, T))
+    end
+
+    return real(s)
 end
