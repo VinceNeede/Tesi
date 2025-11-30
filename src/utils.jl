16dde8c0525e3ca257a36f83880410acbbdbe2bf
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-11-30 10:38:39 +0100

    Add LimitedBiasedNeelState function to create MPS state with localized qubits in specified range

--- src/utils.jl
+++ src/utils.jl
@@ -79,6 +79,19 @@ function BiasedNeelState(sites::IndexSet, θ::Float64)::MPS
 end
 
 """
+    LimitedBiasedNeelState(sites::IndexSet, θ::Float64, range::UnitRange{Int})::MPS
+Construct the initial state of a chain with qubits in state
+`|ψᶿ⟩` at odd sites within `range` and `|0⟩` elsewhere, i.e.,
+`|ψ₀⟩ = |0⟩ ... |0⟩ |ψᶿ⟩ |0⟩ |ψᶿ⟩ ... |0⟩`
+"""
+function LimitedBiasedNeelState(sites::IndexSet, θ::Float64, range::UnitRange{Int})::MPS
+    return MPS([
+        n in range && isodd(n) ? ITensorMPS.state(site, "ψᶿ"; θ = θ) : ITensorMPS.state(site, "Up") for
+        (n, site) in enumerate(sites)
+    ])
+end
+
+"""
     CentralQuasiParticle(sites::IndexSet)::MPS
 Construct an initial MPS state with a quasi-particle localized at the center
 of the chain, in a superposition of moving left and right.
