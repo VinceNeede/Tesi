f86d3fa127c713a6b4b7158214bb301d97d4c186
vince_goldrake <v.bisogno3@studenti.unipit.it>
2026-02-27 21:45:32 +0100

    Refactor main function in start.jl to adjust process parameters and streamline execution

--- start.jl
+++ start.jl
@@ -3,22 +3,20 @@ using Distributed
 include("main.jl")
 
 function @main(_)
-    addprocs(10)
-    θs = 0.001:0.001:0.01
-    γ = 0.16
-    final_time = ceil(Int, 2.5/γ)
-    for th in θs
-        execute(
-            th,
-            γ,
-            "Z",
-            100,
-            2048,
-            final_time,
-            [50],
-            5_000;
-            flush_every=final_time + 1,
-            nthreads=1,
-        )
-    end
+    addprocs(3)
+	tf = 12#ceil(Int, 2.5/γ)
+	for y in [0.5, 0.4]
+		execute(
+			0.5,
+			y,
+			"X",
+			50,
+			4096,
+			tf,
+			3:25,
+			2_000;
+			flush_every=tf + 1,
+			nthreads=5,
+		)
+	end
 end
